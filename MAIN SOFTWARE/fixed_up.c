2ccc4b67ea53671e634c82ea69e28c58a53052ca
owanderson <35957099+owanderson@users.noreply.github.com>
2018-04-09 16:06:53 +0100

    Update fixed_up.c

--- MAIN SOFTWARE/fixed_up.c
+++ MAIN SOFTWARE/fixed_up.c
@@ -15,7 +15,7 @@ sudo ./servo_demo          # Send servo pulses to GPIO 4.
 sudo ./servo_demo 23 24 25 # Send servo pulses to GPIO 23, 24, 25.
 */
 
-/*This code, when called from the main UI, brings a target fixed rotation servo into the down position*/
+/*This code, when called from the main UI, brings a target fixed rotation servo into the up position*/
 
 #define NUM_GPIO 32
 
